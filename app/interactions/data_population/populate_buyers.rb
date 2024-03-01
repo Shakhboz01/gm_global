1a93c1275de53060c76f714daa3d810332ed7198
Shakhboz <shfazliddinov1940@gmail.com>
2024-03-01 11:27:15 +0500

    minor change

--- app/interactions/data_population/populate_buyers.rb
+++ app/interactions/data_population/populate_buyers.rb
@@ -1,7 +1,7 @@
 module DataPopulation
   class PopulateBuyers < ActiveInteraction::Base
     def execute
-      buyers_data = JSON.parse(File.read('app/assets/javascripts/buyers2.json'))
+      buyers_data = JSON.parse(File.read('app/assets/javascripts/buyers.json'))
       buyers_data.each do |buyer_data|
         create_buyers(buyer_data)
       end
@@ -14,7 +14,9 @@ module DataPopulation
         Buyer.create(
           name: data['buyer_name'],
           phone_number: data['phone_number'],
-          active: true
+          active: true,
+          debt_in_uzs: data['debt_in_uzs'],
+          debt_in_usd: data['debt_in_usd']
         )
     end
   end
